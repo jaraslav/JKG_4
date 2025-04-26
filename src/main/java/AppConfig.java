d2bcb949e8847d59977bee937f74e3f911f10deb
jaraslav <Jrikygzz@gmail.com>
2025-04-26 20:18:20 +0300

    ()

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,6 +1,7 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +11,13 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setMessage("Meow-Meow");
+        return cat;
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+    public Cat() {}
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
